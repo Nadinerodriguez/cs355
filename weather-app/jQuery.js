c667bd4ac0591bc31336d8113f86524ff4ad06b0
RodriguezNadine <nadine.rodriguez@gmail.com>
2021-03-29 01:49:52 -0400

    New weather app

--- weather-app/jQuery.js
+++ weather-app/jQuery.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // Get Location 
+    // Get Location
     navigator.geolocation.getCurrentPosition(success, error);
 
     function success(pos) {
@@ -24,13 +24,13 @@ $(document).ready(function () {
     function updateDOM(data) {
         var city = data.name;
         var temp = Math.round(data.main.temp);
-        var feelsLike = Math.round(data.main.feels_like);
+        var humidityVal = data.main.humidity;
         var desc = data.weather[0].description;
         var icon = data.weather[0].icon;
 
         $('#city').html(city);
         $('#temp').html(temp);
-        $('#feels-like-temp').html(feelsLike);
+        $('#humidityVal-temp').html(humidityVal);
         $('#desc').html(desc);
         $('#icon').attr('src', icon);
     }
