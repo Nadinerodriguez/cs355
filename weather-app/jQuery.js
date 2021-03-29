48d36c83373458a9c7aae0d0bedd863bd59285a2
Nadine Rodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2021-03-29 01:22:14 -0400

    Update jQuery.js

--- weather-app/jQuery.js
+++ weather-app/jQuery.js
@@ -25,13 +25,13 @@ $(document).ready(function () {
     function updateDOM(data) {
         var city = data.name;
         var temp = Math.round(data.main.temp);
-        var feelsLike = Math.round(data.main.feels_like);
+        var humidityVal = Math.round(data.main.humidity);
         var desc = data.weather[0].description;
         var icon = data.weather[0].icon;
 
         $('#city').html(city);
         $('#temp').html(temp);
-        $('#feels-like-temp').html(feelsLike);
+        $('#humidity-temp').html(humidityVal);
         $('#desc').html(desc);
         $('#icon').attr('src', icon);
     }
