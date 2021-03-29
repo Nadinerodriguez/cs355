7b4291d579f57ad5409e1788f9411c14d23e374e
Nadine Rodriguez <44532390+Nadinerodriguez@users.noreply.github.com>
2021-03-29 01:13:44 -0400

    Update jQuery.js

--- weather-app/jQuery.js
+++ weather-app/jQuery.js
@@ -12,24 +12,26 @@ $(document).ready(function () {
         console.log('There was an error');
     }
     // Call Weather
-    function weather(lat, long) {
+    function getWeather(lat, long) {
         var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
 
         $.getJSON(URL, function(data) {
+            console.log(data);
             updateDOM(data);
         });
     }
+
     // Update Dom
     function updateDOM(data) {
         var city = data.name;
-        var temp = Math.round(data.main.temp_max);
-        var humidityVal = data.main.humidity;
+        var temp = Math.round(data.main.temp);
+        var feelsLike = Math.round(data.main.feels_like);
         var desc = data.weather[0].description;
         var icon = data.weather[0].icon;
 
         $('#city').html(city);
         $('#temp').html(temp);
-        $('#humidityVal-temp').html(humidityVal);
+        $('#feels-like-temp').html(feelsLike);
         $('#desc').html(desc);
         $('#icon').attr('src', icon);
     }
